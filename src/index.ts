937023d0e5b3c9ceecf49b54b90d412c732f37f9
Pavel Karpenko <ospkiev@gmail.com>
2023-01-31 12:50:06 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {someFunc} from "./test";
+someFunc(123);
+
+document.body.innerHTML = `<div style="background: green">Hello !</div>`
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export function someFunc(arg: number): string {
+    console.log('TEST-77777777777777')
+    return ''
+}
