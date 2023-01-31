3b492c11d0632b4e3eff21acd30949c74aa61c36
Pavel Karpenko <ospkiev@gmail.com>
2023-01-31 13:50:17 +0200

    add_scss_loader_new

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import {someFunc} from "./test";
-someFunc(123);
-
-document.body.innerHTML = `<div style="background: green">Hello !</div>`
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export function someFunc(arg: number): string {
-    console.log('TEST-123456')
-    return ''
-}
