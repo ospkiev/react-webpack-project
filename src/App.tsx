161a732a134d3f57d6af43d7bde59ee81cb2476a
Pavel Karpenko <ospkiev@gmail.com>
2023-02-01 17:01:01 +0200

    6_add_routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
-import './index.scss'
+import React, {Suspense} from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+import './index.scss';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            <p>sfbsbebfebfsbfs</p>
-            <Counter/>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
