359c133795709147e79fcc8ba04573672726909f
Pavel Karpenko <ospkiev@gmail.com>
2023-02-19 14:29:15 +0200

    26_add_loki_screenshot_testing

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,10 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import {BuildOptions} from './types/config';
-import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import { BuildOptions } from './types/config';
 
-export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
         new HtmlWebpackPlugin({
             template: paths.html,
@@ -21,5 +21,5 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new BundleAnalyzerPlugin({
             openAnalyzer: false,
         }),
-    ]
+    ];
 }
